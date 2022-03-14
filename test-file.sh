679bff4c200abffaa3690f767077d98446552095
root <root@Maurice-Kapo.localdomain>
2022-03-14 19:46:45 -0400

    this is the first commit

--- test-file.sh
+++ test-file.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-echo "Enter a first number"
+echo "Detecting wireless issue"
+echo processing..........
+sleep 1
+echo "Resolving the issue"
+echo processing........
 
+network=wireless
 
+i=0
 
-read num1
-sleep 2
-echo "Enter a second number"
-read num2
-sleep 2
-if [[ "$num1" == "$num2" ]];
-then
-	echo "Both numbers are equal"
-elif [[ "$num1" -gt "$num2" ]];
-then
-	echo "$num1 is greater than $num2"
-elif [[ "$num1" -lt "$num2" ]];
-then
-	echo "$num1 is lesser than $num2" 
-else
-	echo " Both numbers are equal to zero"
-fi
+while [ $i -lt 100 ]
 
+do
+
+        i=$((i + 1))
+echo "Solving network issue for "$network  $i"%"
+
+sleep 0.1
+
+done
+
+echo processing......
+sleep 5
+echo processing......
+sleep 5
+
+echo "The wireless issue is resolved!!"
